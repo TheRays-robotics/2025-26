4d7224eec51517da70ffd213264550867a37bbc2
shrimpLiquid <gorgeunthy@gmail.com>
2025-09-18 15:31:52 -0700

    commit

--- float_graphing_UI/main.py
+++ float_graphing_UI/main.py
@@ -1,13 +1,13 @@
 import asyncio
 from pyray import *
-from colorutils import hex_to_rgb as col
+from colorutils import hex_to_rgb
 
 async def main():   # You MUST have an async main function
     init_window(get_screen_width(), get_screen_height(), "Hello")
     while not window_should_close():
         begin_drawing()
-        clear_background(col("000000"))
-        draw_text("Hello world", 190, 200, 20, VIOLET)
+        clear_background(BLACK)
+        draw_text("Hello world", 190, get_mouse_y(), int((get_mouse_x()/10)),LIME)
         end_drawing()
         await asyncio.sleep(0) # You MUST call this in your main loop
     close_window()
