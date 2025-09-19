aeb512d1f3e27aac81beeeb6d7d42b5fc0957115
shrimpLiquid <gorgeunthy@gmail.com>
2025-09-19 07:29:58 -0700

    ?

--- float_graphing_UI/main.py
+++ float_graphing_UI/main.py
@@ -7,12 +7,14 @@ def scale(value, istart, istop, ostart, ostop):
 async def main():   # You MUST have an async main function
     init_window(get_screen_width(), get_screen_height(), "Hello")
     while not window_should_close():
-        print(get_screen_width(), get_screen_height())
         begin_drawing()
         clear_background(BLACK)
         draw_text("Hello world", 190, get_mouse_y(), int((get_mouse_x()/10)),LIME)
+        if abs(10-get_mouse_y()) > 200:
+            draw_text("Hello world", 190, get_mouse_y(), int((get_mouse_x()/10)),VIOLET)
         end_drawing()
         await asyncio.sleep(0) # You MUST call this in your main loop
     close_window()
+    
 
 asyncio.run(main())
