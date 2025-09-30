0fec5dc47fbb5ae8cfb447da4753ab137c25f057
shrimpLiquid <gorgeunthy@gmail.com>
2025-09-30 16:16:38 -0700

    gling

--- float_graphing_UI/main.py
+++ float_graphing_UI/main.py
@@ -6,7 +6,8 @@ X,Y=0,1
 def scale(value, istart, istop, ostart, ostop):
     return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
 
-width,height=get_screen_width(), get_screen_height()
+width,height=1366, 768-45-30
+
 def circleButton(x,y,radius,image,color):
     draw_circle(x,y,radius,color)
     if is_mouse_button_pressed(is_mouse_button_pressed(1)):
@@ -20,8 +21,8 @@ async def graph(data):
     init_window(width, height, "hi")
     font = load_font_ex(("float_graphing_UI/"+mainfont).encode(),300,None,0)
     should_exit = False
-    highests = (0,0)
-    lowests = (0,0)
+    highests = [0,0]
+    lowests = [1000000,100000]
     for point in data:
         if point[X] > highests[X]:
             highests[X] = point[X]
@@ -29,18 +30,19 @@ async def graph(data):
         if point[Y] > highests[Y]:
             highests[Y] = point[Y]
     for point in data:
-        if point[X] > lowests[X]:
+        if point[X] < lowests[X]:
             lowests[X] = point[X]
     for point in data:
-        if point[Y] > lowests[Y]:
+        if point[Y] < lowests[Y]:
             lowests[Y] = point[Y]
 
-    for point in data:
-        draw_circle(scale(point[X],lowests[X],highests[X],33,1360),scale(point[X],lowests[X],highests[X],0,175))
-    
+       
     while not window_should_close() and not should_exit:
         begin_drawing()
-        clear_background(BLACK)
+        clear_background(BLUE)
+        draw_rectangle(33,0,width-33,height-33,(BLACK))
+        for point in data:
+            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),5,LIME)
         #draw_text_ex(font, "Hey Kitten", Vector2(190, get_mouse_y()), int(get_mouse_x()/10) ,int(get_mouse_x()/50), BLUE)
         end_drawing()
         
