1a0646f724d14deea62e72b2981bb1b07007a9a0
shrimpLiquid <gorgeunthy@gmail.com>
2025-09-30 18:44:28 -0700

    gling

--- float_graphing_UI/main.py
+++ float_graphing_UI/main.py
@@ -5,7 +5,7 @@ from math import dist
 X,Y=0,1
 def scale(value, istart, istop, ostart, ostop):
     return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
-
+R_GREEN = Color(89, 208, 55,255)
 width,height=1366, 768-45-30
 
 def circleButton(x,y,radius,image,color):
@@ -16,7 +16,6 @@ def circleButton(x,y,radius,image,color):
     return(False)
     
 mainfont = "mononoki-Regular.ttf"
-
 async def graph(data):
     init_window(width, height, "hi")
     font = load_font_ex(("float_graphing_UI/"+mainfont).encode(),300,None,0)
@@ -39,10 +38,13 @@ async def graph(data):
        
     while not window_should_close() and not should_exit:
         begin_drawing()
-        clear_background(BLUE)
+        clear_background(WHITE)
         draw_rectangle(33,0,width-33,height-33,(BLACK))
         for point in data:
-            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),5,LIME)
+            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),6,BLACK)
+            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),5,R_GREEN)
+            draw_line_ex(Vector2())
+            draw_text_ex(font,str(point[Y]),Vector2(0,scale(point[Y],lowests[Y],highests[Y],0,height-33)),15,2,BLACK)
         #draw_text_ex(font, "Hey Kitten", Vector2(190, get_mouse_y()), int(get_mouse_x()/10) ,int(get_mouse_x()/50), BLUE)
         end_drawing()
         
@@ -65,7 +67,7 @@ async def main():
                 begin_drawing()
                 clear_background(BLACK)
                 #draw_text("Hello world", 190, get_mouse_y(), int((get_mouse_x()/10)), LIME)
-                if circleButton(789,135,25,NotImplemented,BLUE):
+                if circleButton(789,135,25,NotImplemented,R_GREEN):
                     break
                 
 
@@ -84,7 +86,7 @@ async def main():
                 current_state = "graph_window"
                 
         elif current_state == "graph_window":
-            await graph([(0,0),(5,10),(10,5),(15,15)])
+            await graph([(0,0),(5,1),(10,2),(15,3)])
             current_state = "main_window"
     
 
