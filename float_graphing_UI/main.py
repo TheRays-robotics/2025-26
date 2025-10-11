98c215cfb24dae5918a1fb84311949effa7182e4
shrimpLiquid <gorgeunthy@gmail.com>
2025-10-11 13:18:59 -0700

    added y-axis labels

--- float_graphing_UI/main.py
+++ float_graphing_UI/main.py
@@ -2,9 +2,12 @@ import asyncio
 from pyray import *
 import os
 from math import dist
+#run command before file: export DISPLAY=:0
 X,Y=0,1
 def scale(value, istart, istop, ostart, ostop):
     return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+def scaleInt(value, istart, istop, ostart, ostop):
+    return int(ostart + (ostop - ostart) * ((value - istart) / (istop - istart)))
 R_GREEN = Color(89, 208, 55,255)
 width,height=1366, 768-45-30
 
@@ -38,13 +41,21 @@ async def graph(data):
        
     while not window_should_close() and not should_exit:
         begin_drawing()
-        clear_background(WHITE)
+        clear_background(Color(20,50,20))
         draw_rectangle(33,0,width-33,height-33,(BLACK))
+        verticalmarkerdistance = scale(0.5,lowests[Y],highests[Y],0,height-33)
+        for i in range(int(highests[Y]/0.5)):
+            draw_line(0,int(verticalmarkerdistance*i),1366,int(verticalmarkerdistance*i),WHITE)
+            draw_text_ex(font,str(i/2),Vector2(0,int(verticalmarkerdistance)*i),15,2,R_GREEN)
+        draw_text_ex(font,str(highests[Y]),Vector2(0,height-33),15,2,R_GREEN)
+        draw_line(0,height-33,1366,height-33,WHITE)
         for point in data:
-            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),6,BLACK)
-            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),5,R_GREEN)
-            draw_line_ex(Vector2())
-            draw_text_ex(font,str(point[Y]),Vector2(0,scale(point[Y],lowests[Y],highests[Y],0,height-33)),15,2,BLACK)
+            #draw_line(0,scaleInt(point[Y],lowests[Y],highests[Y],0,height-33),1366,scaleInt(point[Y],lowests[Y],highests[Y],0,height-33),WHITE)
+            
+            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),8,BLACK)
+            draw_circle(int(scale(point[X],lowests[X],highests[X],33,width)),int(scale(point[Y],lowests[Y],highests[Y],0,height-33)),7,R_GREEN)
+            
+            #draw_text_ex(font,str(point[Y]),Vector2(0,scale(point[Y],lowests[Y],highests[Y],0,height-33)),15,2,R_GREEN)
         #draw_text_ex(font, "Hey Kitten", Vector2(190, get_mouse_y()), int(get_mouse_x()/10) ,int(get_mouse_x()/50), BLUE)
         end_drawing()
         
