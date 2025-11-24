a3b92f77bb52235580260b566377117bc2e1965c
shrimpLiquid <gorgeunthy@gmail.com>
2025-11-24 09:49:09 -0800

    Update esc.ino

--- thrust_test/esc/esc.ino
+++ thrust_test/esc/esc.ino
@@ -3,14 +3,14 @@ Servo esc;
 void setup() {
   // put your setup code here, to run once:
 esc.attach(9);
-esc.writeMicroseconds(10000);
+esc.writeMicroseconds(20000);
 
- delay(4000);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-esc.writeMicroseconds(1000);
+esc.writeMicroseconds(2000);
 
 }
