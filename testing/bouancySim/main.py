77e11013e7eead9d09c2df1c2df50672aa639e8b
shrimpLiquid <gorgeunthy@gmail.com>
2026-01-08 19:35:27 -0800

    polish

--- testing/bouancySim/main.py
+++ testing/bouancySim/main.py
@@ -1,30 +1,36 @@
 from pyray import *
 
 # Initialization
-init_window(800, 600, "Raylib Buoyancy Simulation")
+init_window(400, 800, "Raylib Buoyancy Simulation")
 set_target_fps(60)
 
 L = 100
 G = 500.0
-S = 0.0  # Must be > G for floating
-pos = Vector2(400, 0)
+S = 700.0  # Must be > G for floating
+W = 400
+Y=0
 V = 0.0
 set_trace_log_level(TraceLogLevel.LOG_ERROR) 
 
 while not window_should_close():
     dt = get_frame_time()
     V += G * dt
-    if pos.y + 20 > L:
-        sub = min(1.0, (pos.y + 20 - L) / 40.0)
-        V -= S * sub * dt
+    if Y + 20 > L:
+        sub = min(1.0, (Y + 20 - L) / 40.0)
+        V -= (S-W) * sub * dt
         V *= 0.95
         
-    pos.y += V * dt
-    print(pos.y-100)
+    Y += V * dt
+    if Y > 780:
+        Y = 780
+        V = 0
+    print(int((((Y-100)/680)*4)*100)/100)
+    
     begin_drawing()
     clear_background(RAYWHITE)
     draw_rectangle(0, L, 800, 800-L, BLUE)
-    draw_rectangle_v(pos, Vector2(40, 40), RED)
+    draw_line(0,525,400,525,BLACK)
+    draw_rectangle(200-20, int(Y)-20, 40, 40, RED)
     end_drawing()
 
 close_window()
