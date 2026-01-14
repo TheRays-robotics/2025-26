21f4445509351d9650855c48135c1882e70a9497
shrimpLiquid <gorgeunthy@gmail.com>
2026-01-13 16:47:07 -0800

    realism2

--- testing/bouancySim/main.py
+++ testing/bouancySim/main.py
@@ -7,47 +7,47 @@ set_target_fps(60)
 doserial = False
 if doserial:    
     ser = serial.Serial('COM5', 9600,timeout=10)
+def getScreenY(y):
+    return((y*170)+L)
+#170pixels = 1M
 L = 100
-G = 1013.0 #9.8 m/s^2
-S = 1013.0 +255.040719353
+G = 9.8
+S = 9.8+3.7765
 M = 0
-Y = 0
+Y = -1
 V = 0.0
 sub = 0
-RM = 0
+
 set_trace_log_level(TraceLogLevel.LOG_ERROR) 
 start=time()
-SH = ((L-Y)/680)*4
 while not window_should_close():
-    if abs(time()-start - 1) < 0.01:
-        print((((L-Y)/680)*4)-SH)
-        pass
+    #L=get_mouse_y()
     if doserial:
-        ser.write((str(int((((L-Y)/680)*4)*100)/100)+"\n").encode())
+        ser.write((str(int(Y*100)/100)+"\n").encode())
         if ser.in_waiting > 0:
             LINE=ser.readline().decode(encoding="utf-8")
             print(LINE)
-            RM=((float(LINE)/180)*300)
+            M=((float(LINE)/180)*300)
+    #print(Y)
     dt = get_frame_time()
-    V += G * (RM+1) * dt
-    if Y + 20 > L:
-        sub = min(1.0, (Y + 20 - L) / 40.0)
+    V += G * dt
+    if int(getScreenY(Y)) > 0:
+        sub = max(0, min(1.0, (getScreenY(Y) + 20 - L) / 40.0))
         
+        print(sub)
         V -= (S) * sub * dt
-        V *= 0.95
-        #V -= (432.578909185*(V*V))
-        
+        V *= 1-(sub*0.05)
+
     Y += V * dt
-    if Y > 780:
-        Y = 780
+    if Y > 4:
+        Y = 4
         V *= -0.5
-    
     begin_drawing()
     clear_background(RAYWHITE)
     draw_rectangle(0, int(L), 800, 800-int(L), BLUE)
     draw_line(0,525,400,525,BLACK)
-    draw_rectangle(200-20, int(Y)-20, 40, 40, RED)
-    draw_text(str(int((((Y-L)/680)*4)*100)/100), 200-20, int(Y)-10 ,20 ,BLACK)
+    draw_rectangle(200-20, int(getScreenY(Y))-20, 40, 40, RED)
+    draw_text(str(int(Y*100)/100), 200-20, int(Y)-10 ,20 ,BLACK)
     end_drawing()
 
 close_window()
